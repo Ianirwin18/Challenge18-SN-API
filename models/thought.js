6374a0ff5ac7538000e0b8915e452e691c6010f8
Ian Irwin <Ianiriwn18@gmail.com>
2022-08-16 20:17:56 -0700

    added models

---
+++ models/thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+const Reaction = require("./Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "You need to write a thought still!",
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => date.toString().match(/[A-Za-z]{3}\s\d{2}\s\d{4}/)[0],
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [Reaction],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
