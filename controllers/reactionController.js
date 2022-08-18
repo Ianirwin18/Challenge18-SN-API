2be70ade7d016b6cf65bdb64394475c872a0d089
Ian Irwin <Ianiriwn18@gmail.com>
2022-08-17 20:33:33 -0700

    starting on controllers

---
+++ controllers/reactionController.js
@@ -0,0 +1,24 @@
+const { user, thought, reaction } = require("../models");
+
+module.exports = {
+  async allReactions(req, res) {
+    try {
+      const reactions = await reaction.find();
+      res.status(200).json(reactions);
+    } catch (err) {
+      res.status(500).json({ message: "allThoughts Error", err });
+    }
+  },
+
+  async singleReaction(req, res) {
+    try {
+      const reaction = await reaction.findOne({ id_: req.params.reactionId });
+      if (!reaction) {
+        return res.status(404).json("Reaction not found");
+      }
+      res.status(200).json(reaction);
+    } catch (err) {
+      res.status(500).json({ message: "singleReaction Error", err });
+    }
+  },
+};
