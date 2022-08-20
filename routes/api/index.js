8b72eca1b32ece947c3034b4f220873ef22b3731
Ian Irwin <Ianiriwn18@gmail.com>
2022-08-20 11:54:45 -0700

    more bug fixes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const userRoutes = require("./useRoutes");
-const thoughtRoutes = require("./thoughtRoutes");
-const reactionRoutes = require("./reactionRoutes");
+const userRoutes = require("./user-routes");
+const thoughtRoutes = require("./thought-routes");
+const reactionRoutes = require("./reaction-routes");
 
 router.use("/users", userRoutes);
 router.use("/thoughts", thoughtRoutes);
